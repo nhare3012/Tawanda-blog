f3a9e3cf46a01e6f73a9ea31e0b8c6718e447797
nhare3012 <nharetawanda@gmail.com>
2021-11-23 12:14:12 +0200

    adding navigation

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const articleRouter = require('./routes/articles')
+
+//  Express App 
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+
+app.use('/articles',articleRouter);
+
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'tawanda',
+        createdAt: Date.now(),
+        decription: 'tawanda nahre'
+
+    }]
+    res.render('articles/index', { articles: articles } )
+
+});
+
+// Listen for request
+app.listen(5000);

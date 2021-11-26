a86f79de571feeae600d677db83dd2ea055c451b
nhare3012 <nharetawanda@gmail.com>
2021-11-26 14:24:52 +0200

    blog restart

--- server.js
+++ server.js
@@ -1,44 +1,23 @@
 var PORT = process.env.PORT || 5000
 const express = require('express');
 const mongoose = require('mongoose');
-const articleRouter = require('./routes/articles')
 
-
-//  Express App 
 const app = express();
 
+// connect to the mongodb databases
 
+const dbURI = 'mongodb+srv://Tumelo:kanyemba@blog.hrnjj.mongodb.net/tawanda-blog?retryWrites=true&w=majority'
 
-app.use(express.static(__dirname + '/public'));
-
-// register view engine
-app.set('view engine', 'ejs');
-
-
-app.use('/articles',articleRouter);
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology:true })
+  
 
 
 
+// middlewares
 
-// Routes
-app.get('/', (req, res) => {
-    const articles = [
-    {
-        title: 'tawanda',
-        createdAt: new Date(),
-        description: 'tawanda nahre'
-
-    },
-    {
-        title: 'tawanda2',
-        createdAt: new Date(),
-        description: 'tawanda nahre2'
-
-    }
-]
-    res.render('articles/index', { articles: articles } )
-
-});
+app.use(express.urlencoded({extend: true}))
+app.use(express.static("public"));
+app.set("view engine", "ejs")
 
 // Listen for request
 app.listen(PORT);

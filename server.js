4d2a3e70512f7b565215296bd6c5b0a0b003a258
nhare3012 <nharetawanda@gmail.com>
2021-12-01 13:30:36 +0200

    cards

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
-const PORT = process.env.PORT || 5000
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+const express = require('express');
+const articleRouter = require('./routes/articles');
 
 
-app.set('view engine', 'ejs')
+const app = express();
+
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use('/articles', articleRouter);
 
 
+app.get('/',  (req, res) => {
+     const articles = [
+       {
+         title: 'My first Article',
+         description:'its a long way to freedom',
+         createdAt: new Date()
+       },
+       {
+        title: 'My first Article2',
+        description:'its a long way to freedom2',
+        createdAt: new Date()
+      },
+      {
+        title: 'My first Article3',
+        description:'its a long way to freedom3',
+        createdAt: new Date()
+      },
+      {
+        title: 'My first Article3',
+        description:'its a long way to freedom3',
+        createdAt: new Date()
+      },
+     ]
+     res.render('index', {articles: articles})
+  })
 
-app.listen(PORT)
+
+app.listen(5000)

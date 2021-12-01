6cfc20ee182ce68e2f1531b104ceadd8ed408ac4
nhare3012 <nharetawanda@gmail.com>
2021-12-01 09:56:19 +0200

    starting over

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const Blog = require("../models/Blog");
-
-router.get("/", async (req, res) => {
-  const allBlogs = await Blog.find();
-  res.render("index", { blogs: allBlogs });
-});
-
-module.exports = router;

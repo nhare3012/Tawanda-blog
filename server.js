9b2fffa2bd7d2e3eeca1de4b21a1cf7f3706c93c
nhare3012 <nharetawanda@gmail.com>
2021-12-03 09:13:52 +0200

    delete button

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ console.error(err);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: false }))
